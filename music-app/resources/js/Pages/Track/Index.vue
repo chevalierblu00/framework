fab5838da6381025529659bedb8778d7b78a53e6
chevalierblu00 <mgaspard.perso@gmail.com>
2024-07-09 09:22:07 +0200

    debut du project

---
+++ music-app/resources/js/Pages/Track/Index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Listes des tracks</h1>
+
+        <ul>
+            <li v-for="track in tracks" ::key="track.uuid">
+                {{ track.title }} ({{ track.artist }})
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import MusicLayout from '@/Layouts/MusicLayout.vue';
+
+export default{
+    name: 'Index',
+    components: {
+        MusicLayout,
+    },
+    props: {
+        tracks: Array,
+    },
+}
+</script>
